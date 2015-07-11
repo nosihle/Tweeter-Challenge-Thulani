29e4a5f39023c91c7328582c235146a90384a93c
Thulani Tsabedze <Thulani@Thulani-Tsabedzes-MacBook-Pro.local>
2015-07-11 01:31:04 -0400

    Shell script now returns to the working directory after running programs

--- run.sh
+++ run.sh
@@ -2,8 +2,18 @@
 
 #This script runs the programes to determine unique tweets together with median for a page of tweets.
 
+# Change directory to the where the source code is located
 cd /Users/Thulani/Desktop/Tweeter-Challenge-Thulani/src
+
+# Remember the working directory 
+path=$(cd -) 
+
+# Compile and run the following python programs
+
 python words_tweeted.py
 
 python median_unique.py
 
+# Change back to the directory you originally were.
+
+cd $path
